9d4c763c9e0fe710aa8a4654f1fd469957ea8816
k2 <izmail.k2@gmail.com>
2025-02-24 12:50:17 +0300

    feat: add ImageEdit

---
+++ src/features/imageEdit/ImageEdit.tsx
@@ -0,0 +1,68 @@
+import React, {useState} from 'react';
+import {StyleSheet, View} from "react-native";
+import {ImageViewer} from "common/components/ImageViewer";
+import {ButtonCustom} from "common/components/ButtonCustom";
+import {Entypo} from "@expo/vector-icons";
+import * as ImagePicker from "expo-image-picker";
+
+const PlaceholderImage = require('assets/images/background-image.png');
+
+export const ImageEdit = () => {
+  const [selectedImage, setSelectedImage] = useState<string | null>(null) //для сохранения состояния изображения
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true, //можно обрезать изображение при установке на телефонах, но не в web
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri) //save image in value selectedImage
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+
+  return (
+    <View>
+      <View style={styles.imageContainer}>
+        <ImageViewer image={PlaceholderImage} selectedImage={selectedImage}/>
+      </View>
+
+      <View style={styles.footerContainer}>
+
+        <ButtonCustom name={'Choose a photo'} style={styles.bcButton} callback={pickImageAsync}>
+          <Entypo name="images" size={24} color="black" style={styles.buttonIcon}/>
+        </ButtonCustom>
+
+        <ButtonCustom name={'Use this photo'} callback={() => alert('hi')}/>
+
+      </View>
+
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
+  buttonIcon: {
+    paddingRight: 8,
+  },
+
+  bcButton: {
+    backgroundColor: "#98b4d9",
+    color: "#000",
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+
+})
