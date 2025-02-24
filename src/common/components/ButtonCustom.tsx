52529c8ed2e46df47cb39f6a47c8e0194f786044
k2 <izmail.k2@gmail.com>
2025-02-24 18:31:40 +0300

    style: change

--- src/common/components/ButtonCustom.tsx
+++ src/common/components/ButtonCustom.tsx
@@ -1,20 +1,18 @@
-import React, {ReactNode} from 'react';
-import {Pressable, View, StyleSheet, Text, ViewStyle} from "react-native";
-
+import React, { ReactNode } from "react";
+import { Pressable, View, StyleSheet, Text, ViewStyle } from "react-native";
 
 type Props = {
-  name?:string,
-  style?: ViewStyle,
-  children?: ReactNode,
-  callback: () => void
-}
-export const ButtonCustom = ({children, callback, name, style}: Props) => {
-
-
+  name?: string;
+  styleBtn?: ViewStyle;
+  styleText?: { color: string };
+  children?: ReactNode;
+  callback: () => void;
+};
+export const ButtonCustom = ({ children, callback, name, styleText, styleBtn }: Props) => {
   return (
     <View style={styles.buttonContainer}>
-      <Pressable style={[styles.button, style]} onPress={callback}>
-        <Text style={[styles.buttonLabel, style]}>{name}</Text>
+      <Pressable style={[styles.button, styleBtn]} onPress={callback}>
+        <Text style={[styles.buttonLabel, styleText]}>{name}</Text>
         {children}
       </Pressable>
     </View>
@@ -26,20 +24,20 @@ const styles = StyleSheet.create({
     width: 320,
     height: 68,
     marginHorizontal: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     padding: 3,
   },
   button: {
     borderRadius: 10,
-    width: '100%',
-    height: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'row-reverse',
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row-reverse",
   },
   buttonLabel: {
-    color: '#98b4d9',
+    color: "#98b4d9",
     fontSize: 16,
   },
 });
